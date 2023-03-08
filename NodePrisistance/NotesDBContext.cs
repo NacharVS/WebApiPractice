318649f955efed6c409273212ce3f872fac369ac
nacharvs <r3dinc@yandex.ru>
2023-03-08 19:59:00 +0300

    EntityFrameWork

---
+++ NodePrisistance/NotesDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApplication.Interfaces;
+using NotesDomain;
+using NodePrisistance.EntityTypeConfigurations;
+
+namespace NodePrisistance
+{
+    public class NotesDBContext : DbContext, INotesDBContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDBContext(DbContextOptions<NotesDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+        public Task<int> SaveChangeAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NotesApplication/Interfaces/INotesDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NotesDomain;
+namespace NotesApplication.Interfaces
+{
+    public interface INotesDBContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangeAsync(CancellationToken cancellationToken);
+    }
+}
